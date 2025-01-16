0b407538acc682121439bbe52e55ab7558cba979
Mishad <mishadcs2002@gmail.com>
2025-01-16 17:06:11 +0600

    Assets and Resources Added

---
+++ edu_bridge_app/lib/resources/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color bg = Color(0xfff8fcfc);
+  static const Color themeColor = Color(0xff0961F5);
+  static const Color white = Color(0xffFFFFFF);
+  static const Color blackGray = Color(0xff545454);
+}
---
+++ edu_bridge_app/lib/resources/assets_path.dart
@@ -0,0 +1,26 @@
+class AssetsPath {
+  static const String _asset = 'assets';
+  static const String images = '$_asset/images';
+  static const String icons = '$_asset/icons';
+  static const String logo = '$_asset/logo';
+
+  //Logo
+  static const String appLogo = '$logo/logo.png';
+  static const String school = '$logo/1_school.svg';
+  static const String college = '$logo/2_college.svg';
+  static const String english = '$logo/3_english.svg';
+  static const String skill = '$logo/4_skill.svg';
+  static const String health = '$logo/5_health.svg';
+  static const String creative = '$logo/6_creative.svg';
+  static const String life = '$logo/7_life.svg';
+
+  //Icons
+  static String google = '$icons/google_logo.svg';
+  static String apple = '$icons/apple_logo.svg';
+  static String sliderCard = '$images/offer.png';
+
+  static String google1 =
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/1200px-Google_%22G%22_logo.svg.png";
+  static String apple2 =
+      "https://e7.pngegg.com/pngimages/912/682/png-clipart-apple-logo-brand-apple-company-trademark-thumbnail.png";
+}
