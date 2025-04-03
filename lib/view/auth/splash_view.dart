dcbb06b1cee285dabc10772e06f195702b2d4e1f
Mishad <mishadcs2002@gmail.com>
2025-04-03 15:34:26 +0600

    Restructure project files and directories

---
+++ lib/view/auth/splash_view.dart
@@ -0,0 +1,86 @@
+import 'package:edu_bridge_app/resources/export.dart';
+import 'package:edu_bridge_app/view/home/home_view.dart';
+import 'package:edu_bridge_app/view/on_boarding/on_boarding.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    _fadeAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: Curves.easeInOut,
+      ),
+    );
+
+    _controller.forward();
+    _controller.addStatusListener((status) {
+      if (status == AnimationStatus.completed) {
+        _controller.reverse();
+      }
+    });
+    _moveToNextScreen();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Future<void> _moveToNextScreen() async {
+    await Future.delayed(const Duration(seconds: 3));
+
+    final supabase = Supabase.instance.client;
+    final session = supabase.auth.currentSession;
+
+    if (session != null && session.user != null) {
+      String email = session.user!.email ?? ''; // Retrieve the email
+      Get.offAll(HomeView(email: email)); // Pass email to HomeView
+    } else {
+      Get.offAll(OnBoarding());
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.white,
+      body: Padding(
+        padding: const EdgeInsets.all(50.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+              animation: _fadeAnimation,
+              builder: (context, child) {
+                return Opacity(
+                  opacity: _fadeAnimation.value,
+                  child: Image.asset(AssetsPath.appLogo2),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
