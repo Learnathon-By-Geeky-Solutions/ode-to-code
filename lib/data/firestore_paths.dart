b41a6f78d0d74e443e7842bb401275d58359bf9d
Sf <110230611+saif-gitreps@users.noreply.github.com>
2025-04-07 22:46:03 +0600

    Optimized code and removed unnecessary files

--- lib/data/firestore_paths.dart
+++
@@ -1,6 +0,0 @@
-class FirestorePaths {
-  static String categoriesCollection() => 'categories';
-  static String chaptersCollection(
-          String categoryId, String classId, String subjectId) =>
-      'categories/$categoryId/classes/$classId/subjects/$subjectId/chapters';
-}
